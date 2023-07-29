65d3d173f4881d55adc0cae831eb550412e1fb5e
tristanlatr <tristanlatr@users.noreply.github.com>
2023-07-29 10:42:48 -0400

    Add visit_Index

--- libstatic/asteval.py
+++ libstatic/asteval.py
@@ -483,7 +483,14 @@ class _LiteralEval(_ASTEval):
             else None
         )
         return slice(lower, upper, step)
+    
+    if sys.version_info < (3,9):
 
-    def visit_NamedExpr(self, node: ast.NamedExpr, path: List[ast.AST]) -> LiteralValue:
-        return ensure_literal(self.visit(node.value, path))
+        def visit_Index(self, node: ast.Index, path: List[AST]) -> ASTOrLiteralValue:
+            return self.visit(node.value, path)
+    
+    if sys.version_info >= (3,9):
+
+        def visit_NamedExpr(self, node: ast.NamedExpr, path: List[ast.AST]) -> LiteralValue:
+            return ensure_literal(self.visit(node.value, path))
 
