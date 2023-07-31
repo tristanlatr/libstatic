57523bc2f0452d9ab6f1e9f7e3ebde621b9ae612
tristanlatr <tristanlatr@users.noreply.github.com>
2023-07-31 02:14:44 -0400

    better cli

--- libstatic/__main__.py
+++ libstatic/__main__.py
@@ -1,22 +1,36 @@
 import ast
 import sys
 from pathlib import Path
+import argparse
 
 from .model import Project
 from .loader import load_path
 
-paths = sys.argv[1:]
-proj = Project(verbosity=1)
-assert paths, 'missing positional argument: PATH, [PATH]'
+def main():
+    parser = argparse.ArgumentParser(description='',
+                                     prog='python3 -m libstatic',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-for path in paths:
-    p = Path(path)
-    assert p.exists(), f'source path does not exist: {p}'
-    load_path(proj, p)
+    parser.add_argument('PATH', help='path to python modules/packages', nargs='+')
+    parser.add_argument('-v', '--verbose', dest='verbosity', default=0, action='count', help='increase verbosity', )
+    parser.add_argument('-d', '--dependencies', default=0, type=int, help='try to load nested dependencies')
+    args = parser.parse_args()
 
-proj.analyze_project()
-total_defs = len(proj.state._def_use_chains)
-mod_defs = len([o for o in proj.state._def_use_chains if isinstance(o, ast.Module)])
+    proj = Project(verbosity=args.verbosity, 
+                   nested_dependencies=args.dependencies, 
+                   python_version=tuple(sys.version_info[:2]))
 
-print(f'Project has {total_defs} use or definitions in the chains')
-print(f'Project has {mod_defs} modules in the chains')
+    for path in args.PATH:
+        p = Path(path)
+        assert p.exists(), f'source path does not exist: {p}'
+        load_path(proj, p)
+
+    proj.analyze_project()
+    total_defs = len(proj.state._def_use_chains)
+    mod_defs = len([o for o in proj.state._def_use_chains if isinstance(o, ast.Module)])
+
+    print(f'Project has {total_defs} use or definitions in the chains')
+    print(f'Project has {mod_defs} modules in the chains')
+
+if __name__ == "__main__":
+    main()
