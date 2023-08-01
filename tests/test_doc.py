2d1e599c4225e00e97eaee93362f6ae970888b29
tristanlatr <tristanlatr@users.noreply.github.com>
2023-08-01 14:51:21 -0400

    test doctests

---
+++ tests/test_doc.py
@@ -0,0 +1,16 @@
+import doctest
+import unittest
+import os
+from importlib import import_module
+from .importlib_utils import get_submodules
+
+import libstatic.model
+
+class TestDoctest(unittest.TestCase):
+    def test_lib_doctests(self):
+        failed, _ = doctest.testmod(libstatic.model, optionflags=doctest.ELLIPSIS)
+        self.assertEqual(failed, 0)
+
+    def test_readme(self):
+        failed, _ = doctest.testfile(os.path.join("..", "README.md"))
+        self.assertEqual(failed, 0)
