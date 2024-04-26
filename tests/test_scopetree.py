37c5eef3604f835c1552ce15f18c8153914578c5
tristanlatr <tristanlatr@users.noreply.github.com>
2024-04-26 11:14:23 -0400

    Add scopetree.py (and fix it) taken from Guido's blog

---
+++ tests/test_scopetree.py
@@ -0,0 +1,76 @@
+
+
+import ast
+from textwrap import dedent
+from unittest import TestCase
+
+from libstatic._lib.scopetree import Builder, GlobalScope, ClassScope, FunctionScope
+
+class TestPassManagerFramework(TestCase):
+    def test_scopes(self, ):
+        # Set up a sample program
+        # class C:
+        #   def foo(self, a = blah):
+        #     global x
+        #     x = a
+
+        globals = GlobalScope()
+        c = ClassScope("C", globals)
+        foo = FunctionScope("foo", c)
+        foo.store("self")
+        foo.store("a")
+        foo.add_global("x")
+
+        assert foo.lookup("C") is globals
+        assert c.lookup("C") is globals
+
+        assert foo.lookup("foo") is None
+        assert c.lookup("foo") is c
+
+        assert foo.lookup("self") is foo
+        assert c.lookup("self") is None
+
+        assert foo.lookup("a") is foo
+        assert c.lookup("a") is None
+
+        assert foo.lookup("blah") is None
+        assert c.lookup("blah") is None
+
+        assert foo.lookup("x") is globals
+        assert c.lookup("x") is None
+    
+    def test_builder(self, ):
+        src = '''
+        class C:
+          def foo(self, a = blah):
+            global x
+            x = a
+        '''
+        node = ast.parse(dedent(src))
+        builder = Builder()
+        builder.build(node)
+
+        globals = builder.globals
+        c = next(iter(s for s in builder.scopes if isinstance(s, ClassScope)))
+        foo = next(iter(s for s in builder.scopes if isinstance(s, FunctionScope)))
+        assert c and foo
+       
+        assert foo.lookup("C") is globals
+        assert c.lookup("C") is globals
+
+        assert foo.lookup("foo") is None
+        assert c.lookup("foo") is c
+
+        assert foo.lookup("self") is foo
+        assert sorted(globals.locals) == ['C', ]
+        assert sorted(c.locals) == ['foo', ]
+        assert c.lookup("self") is None
+
+        assert foo.lookup("a") is foo
+        assert c.lookup("a") is None
+
+        assert foo.lookup("blah") is None
+        assert c.lookup("blah") is None
+
+        assert foo.lookup("x") is globals
+        assert c.lookup("x") is None
