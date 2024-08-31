0d83c9d1c08c0ff96af228d17a0c0ba425323fa1
tristanlatr <tristanlatr@users.noreply.github.com>
2024-08-31 11:18:16 -0400

    Update setup.py with a few todos

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from setuptools import setup, find_packages
 setup( 
     name = "libstatic",
     version = "0.2.0.dev3",
-    description = "Static analysis library based on beginet",
+    description = "Static analysis framework",
     author = "Various Developers",
     url = "https://github.com/tristanlatr/libstatic",
     packages = find_packages(exclude=['tests']),
     install_requires = [
         'attrs',
         'typeshed_client',
+        # TODO: Finish the work on beniget and use the upstream version 
         # 'beniget @ git+https://github.com/serge-sans-paille/beniget.git',
         'beniget @ git+https://github.com/pyforks/beniget-ng.git',
-        'astor',
+        'astor', # TODO: Drop python 3.8 and remove the astor dependency.
         ],
     python_requires='>=3.7',
     )
