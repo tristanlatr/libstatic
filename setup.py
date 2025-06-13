217b71ef33907e8cd4c602782d0f90c1960a8f88
tristanlatr <tristanlatr@users.noreply.github.com>
2025-06-13 15:19:28 -0400

    Hopefully fix publishing

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 setup( 
     name = "libstatic",
     version = "0.2.0.dev3",
-    description = "Static analysis library based on beginet",
-    author = "Various Developers",
+    description = "Static analysis framework",
+    maintainer = "tristanlatr",
     url = "https://github.com/tristanlatr/libstatic",
     packages = find_packages(exclude=['tests']),
     install_requires = [
